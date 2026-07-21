9472020df2baf76bebfba0777cdb8ffb6379a3f5
Rokoudram <merlin.olivry.34@gmail.com>
2026-07-22 00:47:07 +0200

    Amélioration barre navigation Création root Amélioration liens type card

---
+++ config/liste-jeux.ts
@@ -0,0 +1,18 @@
+export const sections = [
+    {
+        title: "La bataille des jardiniers",
+        href: "/jeux-maths/jeux/jardiniers",
+        description:
+            "Une version revisitée de la bataille navale.",
+        nav: true,
+        nav_mobile:true,
+    },
+    {
+        title: "Curvle",
+        href: "/jeux-maths/jeux/curvle",
+        description:
+        "Deviner l'expression d'une fonction à l'aide de sa courbe représentative.",
+        nav: true,
+        nav_mobile:true,
+    },
+];
---
+++ config/sections.ts
@@ -0,0 +1,26 @@
+export const sections = [
+    {
+        title: "Jeux",
+        href: "/jeux-maths/jeux",
+        description:
+            "Page d'accueil des jeux.",
+        nav: true,
+        nav_mobile:true,
+    },
+    {
+        title: "À propos",
+        href: "/jeux-maths/about",
+        description:
+        "Informations relatives au site.",
+        nav: true,
+        nav_mobile:true,
+    },
+    {
+        title: "Contact",
+        href: "/jeux-maths/contact",
+        description:
+        "Me contacter",
+        nav: true,
+        nav_mobile:true,
+    },
+];
